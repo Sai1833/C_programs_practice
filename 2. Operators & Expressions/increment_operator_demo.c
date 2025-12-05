7df9ae44fa87d766de1f66e0623f8bf38540e69e
Sai <157356123+Sai1833@users.noreply.github.com>
2025-12-05 15:45:48 +0530

    2.4

---
+++ 2. Operators & Expressions/increment_operator_demo.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int a = 5;
+    printf("Initial value: %d\n", a);
+    printf("Post-increment a++: %d\n", a++);
+    printf("After post-increment: %d\n", a);
+    printf("Pre-increment ++a: %d\n", ++a);
+
+    return 0;
+}
