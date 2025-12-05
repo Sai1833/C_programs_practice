f8261f32c5c48823f3b1862e4d0e81b421d6b8c4
Sai <157356123+Sai1833@users.noreply.github.com>
2025-12-05 16:34:56 +0530

    7.1

---
+++ 7. Matrices (2D Arrays)/matrix_input_output.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r, c;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter %d elements:\n", r * c);
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Matrix is:\n");
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++)
+            printf("%d ", a[i][j]);
+        printf("\n");
+    }
+
+    return 0;
+}
